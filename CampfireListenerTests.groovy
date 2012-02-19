853accb5ae5da6147715b392fe57850b14764e3d
Steve Stodola <steve@plytro.com>
2012-02-19 09:23:04 -0700

    Add test for issue created case

--- CampfireListenerTests.groovy
+++ CampfireListenerTests.groovy
@@ -33,4 +33,30 @@ class CampfireListenerTest extends GroovyTestCase{
    cfListener.workflowEvent(issueEvent)
  }
 
+ void testIssueCreatedToCampfire(){
+   def cfListener = new CampfireListener();
+   cfListener.campfireKey=""
+   cfListener.campfireRoom="000000"
+   cfListener.campfireBaseUrl="https://COMAPNY.campfirenow.com/"
+   cfListener.issueBaseUrl="http://jira.COMPANY.com/browse"
+   
+   def mIssue = new MockIssue()
+   mIssue.key = "MOCK-001"
+   mIssue.summary = "Mock Summary"
+   mIssue.description = "Mock description"
+
+   def mUser = new MockUser()
+   mUser.setDisplayName("Mock User")
+
+   def mWorklog = new MockWorklog()
+   def mComment = new MockComment()
+   mComment.body = "Mock Comment"
+
+//   org.ofbiz.core.entity.GenericValue mChangeGroup = null
+//   def issueEvent = new IssueEvent(mIssue, mUser, mComment, mWorklog, null, new java.util.HashMap(), ISSUE_COMMENTED_ID)
+   def issueEvent = new IssueEvent(mIssue, new java.util.HashMap(), mUser, ISSUE_CREATED_ID)
+   cfListener.workflowEvent(issueEvent)
+ }
+
+
 }
